6fd0bda002291affd01f179e3b53cff654fa7137
Nitik sharma <sharmanitik36@gmail.com>
2024-02-17 15:52:44 +0530

    complete

--- script.js
+++ script.js
@@ -21,15 +21,32 @@ textArea.addEventListener("click", () => {
   addTextArea();
 });
 
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+function drop(event) {
+  event.preventDefault();
+  const data = event.dataTransfer.getData("text");
+  const draggedElement = document.getElementById(data);
+  const container = document.getElementById("form-container");
+  container.insertBefore(draggedElement, event.target);
+}
+
 // Function for add input text
 const addInputText = () => {
   var inputElement = document.createElement("input");
+
   inputElement.classList.add("input-box");
   inputElement.type = "text";
   inputElement.name = "dynamic-input";
   inputElement.placeholder = "Sample Placeholder";
   inputElement.id = generateId();
   inputElement.draggable = true;
+  inputElement.ondragstart = function (event) {
+    event.dataTransfer.setData("text/plain");
+    event.dataTransfer.setData("text", inputElement.type);
+  };
 
   var label = document.createElement("label");
   label.innerHTML = "Simple label";
@@ -44,9 +61,14 @@ const addInputText = () => {
 // Function to add select Input
 const addSelectInput = () => {
   var selectElement = document.createElement("select");
+
   selectElement.classList.add("select");
   selectElement.id = generateId();
   selectElement.draggable = true;
+  selectElement.ondragstart = function (event) {
+    event.dataTransfer.setData("text/plain");
+    event.dataTransfer.setData("text", selectElement.type);
+  };
 
   for (let i = 1; i <= 3; i++) {
     const option = document.createElement("option");
@@ -66,12 +88,17 @@ const addSelectInput = () => {
 
 const addTextArea = () => {
   const textArea = document.createElement("textarea");
+
   textArea.id = generateId();
   textArea.name = "w3review";
   textArea.rows = "4";
   textArea.cols = "100";
   textArea.placeholder = "Sample Placeholder";
   textArea.draggable = true;
+  textArea.ondragstart = function (event) {
+    event.dataTransfer.setData("text/plain");
+    event.dataTransfer.setData("text", textArea.type);
+  };
 
   const label = document.createElement("label");
   label.innerText = "TextArea";
@@ -86,8 +113,10 @@ const addTextArea = () => {
 const createContainer = () => {
   const divElement = document.createElement("div");
   divElement.classList.add("input-text");
-  divElement.ondrop = "drop(event)";
-  divElement.ondragover = "allowDrop(event)";
+  divElement.draggable = true;
+  divElement.ondragstart = function (event) {
+    event.dataTransfer.setData("text/plain", divElement.id);
+  };
   const innerDiv = document.createElement("div");
   innerDiv.classList.add("inner-input-text");
   divElement.appendChild(innerDiv);
@@ -156,3 +185,25 @@ const saveToJson = () => {
   const jsonContent = JSON.stringify(formElements, null, 2);
   console.log(jsonContent);
 };
+
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+function drop(event) {
+  event.preventDefault();
+  const data = event.dataTransfer.getData("text");
+  const draggedElement = document.getElementById(data);
+  const container = event.currentTarget;
+
+  if (draggedElement && container) {
+    // Create a clone of the dragged element
+    const clonedElement = draggedElement.cloneNode(true);
+
+    // Remove the id attribute to avoid duplicates
+    clonedElement.removeAttribute("id");
+
+    // Append the cloned element to the container
+    container.appendChild(clonedElement);
+  }
+}
